592358ca7f40d4176d1a9e6108d6ed249b5c5149
Madhav-Tiwary <madhavtiwaria2z@gmail.com>
2022-11-24 03:44:32 +0530

    2nd-Day
    Write a program to add two umbers

---
+++ 02_Add_Two_Numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,addition;
+    printf("Enter the first Number :\n");
+    scanf("%d",&num1);
+    printf("Enter the second Number :\n");
+     scanf("%d",&num2);
+    addition=num1+num2;
+    printf("The sum of these two numbers given by you is : %d\n",addition);
+    
+    return 0;
+}
