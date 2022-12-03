8ef72b441a35a4268e8275669ee1fdcd169173d6
Madhav-Tiwary <madhavtiwaria2z@gmail.com>
2022-12-03 22:46:40 +0530

    10th-Day

---
+++ 10_Switch_case.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2, addition, number;
+    printf("Enter any number \nfor<< CHECK YOUR FUTURE >>:");
+    scanf("%d", &number);
+    switch (number)
+    {
+    case 1:
+
+        printf(" Mehnat karo bhagwan tumhare sath hai");
+        printf(" OR THODA KAM HILAYA KARO BSDK ");
+
+        break;
+
+    case 2:
+          printf(" Mehnat karo bhagwan tumhare sath hai");
+        printf(" OR THODA KAM GALIYA DIYA KARO BSDK ");
+
+        break;
+
+    case 3:
+          printf(" Mehnat karo bhagwan tumhare sath hai");
+        printf(" OR DAILY NAHAYA KARO BSDK ");
+
+        break;
+    case 4:
+          printf(" Mehnat karo bhagwan tumhare sath hai");
+        printf(" OR THODA KAM TADA  KARO LADKIYO  BSDK ");
+
+        break;
+    case 5:
+          printf(" Mehnat karo bhagwan tumhare sath hai");
+        printf(" OR THODA YOU TUBE KAM CHALAYA KARO BSDK ");
+
+        break;
+    case 6:
+          printf(" Mehnat karo bhagwan tumhare sath hai");
+        printf(" OR REEL KAM DHEKHA KARO BSDK ");
+
+        break;
+    case 7:
+          printf(" Mehnat karo bhagwan tumhare sath hai");
+        printf(" OR THODA KAM SOYA KARO BSDK ");
+
+        break;
+    case 8:
+          printf(" Mehnat karo bhagwan tumhare sath hai");
+        printf(" OR THODA KAM SOYA KARO BSDK ");
+
+        break;
+    case 9:
+          printf(" Mehnat karo bhagwan tumhare sath hai");
+        printf(" OR THODA KAM SOYA KARO BSDK ");
+
+        break;
+    }
+
+    return 0;
+}
