b29023e59f1f036e2e6c6d9bef21c56fe8a0e70b
Madhav-Tiwary <madhavtiwaria2z@gmail.com>
2022-12-04 03:36:16 +0530

    22th-Day

---
+++ 22_F_No_R_No_Arg.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int name(); // function declaration or prototype
+void main()
+{
+    name();
+}
+int name() // function defination
+{
+
+    printf("Hy i am Madhav Tiwari\n");
+}
