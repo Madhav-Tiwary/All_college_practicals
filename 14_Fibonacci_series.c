90291960b7ac453d4cb0d8d853490534afd2a972
Madhav-Tiwary <madhavtiwaria2z@gmail.com>
2022-12-04 03:02:29 +0530

    14th-Day

--- 14_Fibonacci_series.c
+++ 14_Fibonacci_series.c
@@ -1,18 +1,18 @@
 // see the fibonacci series program in c without recursion.
 
-#include<stdio.h>    
-int main()    
-{    
- int n1=0,n2=1,n3,i,number;    
- printf("Enter the number of elements:");    
- scanf("%d",&number);    
- printf("\n%d %d",n1,n2);//printing 0 and 1    
- for(i=2;i<number;++i)//loop starts from 2 because 0 and 1 are already printed    
- {    
-  n3=n1+n2;    
-  printf(" %d",n3);    
-  n1=n2;    
-  n2=n3;    
- }  
-  return 0;  
- }   
+#include <stdio.h>
+int main()
+{
+    int n1 = 0, n2 = 1, n3, i, number;
+    printf("Enter the number of elements:");
+    scanf("%d", &number);
+    printf("\n%d %d", n1, n2);   // printing 0 and 1
+    for (i = 2; i < number; ++i) // loop starts from 2 because 0 and 1 are already printed
+    {
+        n3 = n1 + n2;
+        printf(" %d", n3);
+        n1 = n2;
+        n2 = n3;
+    }
+    return 0;
+}
