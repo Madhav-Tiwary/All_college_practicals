68c52b9a11610a14fbb6bed080072dfae24c8212
Madhav-Tiwary <madhavtiwaria2z@gmail.com>
2022-12-05 00:48:13 +0530

    28th-Day

---
+++ 28_string.c
@@ -0,0 +1,15 @@
+// write a programme to perform operation on a string
+
+#include <stdio.h>
+int main()
+{
+    char demo1[10];
+    char demo2[10];
+    printf("Enter the First Name :");
+    scanf("%s", demo1);
+    printf("Enter the second Name :");
+    scanf("%s", demo2);
+    printf("%s %s", demo1, demo2);
+
+    return 0;
+}
